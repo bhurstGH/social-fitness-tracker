69b8092787413ca12797823b2fc295d295373271
Brett Hurst <42450547+bhurstGH@users.noreply.github.com>
2019-03-17 18:04:46 -0700

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-//Key from MongoDB Atlas
-module.exports = {
-    mongoURI: 'mongodb://fitAdmin:fitPassword@fitnessapp-shard-00-00-pvy1e.mongodb.net:27017,fitnessapp-shard-00-01-pvy1e.mongodb.net:27017,fitnessapp-shard-00-02-pvy1e.mongodb.net:27017/test?ssl=true&replicaSet=FitnessApp-shard-0&authSource=admin&retryWrites=true'
-}
