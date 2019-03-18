8c6f2f8377bbb7f35f1833fd78d9cf5209221475
Brett Hurst <bretthurst@outlook.com>
2019-03-18 13:52:03 -0700

    Restructure for Heroku.

---
+++ config/routes-config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = (app, express) => {
+  // Check for production environment and direct to React
+  if (process.env.NODE_ENV === "production") {
+    // Serve static files from React build
+    app.use(express.static(path.join(__dirname, "client", "build")));
+
+    // Serve unhandled paths to React index.html
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+  }
+};
