4b907abebb4fad26887ec912b33aa9958475e4f5
Brett Hurst <bretthurst@outlook.com>
2019-03-22 01:34:16 -0700

    Added User validation to fields.

--- models/User.js
+++ models/User.js
@@ -4,16 +4,20 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   username: {
     type: String,
-    required: true
+    required: [true, "Username required."],
+    minLength: [6, "Username must be at least 6 characters long."],
+    maxLength: [25, "Username canot be longer than 25 characters."],
+    trim: true
   },
   email: {
     type: String,
-    required: true,
-    unique: true
+    required: [true, "Email address required"],
+    unique: [true, "That email address is already in use."]
   },
   password: {
     type: String,
-    required: true
+    required: [true, "Password required."],
+    minxLength: [6, "Password must be at least 6 characters long."]
   },
   register_date: {
     type: Date,
