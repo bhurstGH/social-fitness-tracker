a8c191f2a231f57f73ed2f5f03c25e7cac770ee7
Brett Hurst <bretthurst@outlook.com>
2019-03-29 17:48:49 -0700

    Moved static to server.js

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app = require("./app");
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
